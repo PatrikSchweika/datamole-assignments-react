1b6a01c492a7996a34d15d01db6be549e0d88393
paschw <patrik.schweika@hidglobal.com>
2025-02-18 12:36:35 +0100

    F7: Sort the todo items

--- client/src/components/HomePage.tsx
+++ client/src/components/HomePage.tsx
@@ -3,9 +3,31 @@ import { List } from "./List";
 import { Footer } from "./Footer";
 import { useCreateTodo, useDeleteTodo, useTodos, useUpdateTodo } from "./queries/todo-queries";
 import { ListItem } from "./ListItem";
+import { useMemo } from "react";
+import { Todo } from "../models/todo";
+
+const todoComparator = (a: Todo, b: Todo) => {
+    if (a.isDone && !b.isDone) {
+        return 1;
+    }
+
+    if (!a.isDone && b.isDone) {
+        return -1;
+    }
+
+    return b.createdAt - a.createdAt;
+};
 
 export const HomePage = () => {
     const { data: todos } = useTodos();
+
+    const sortedTodos = useMemo(() => {
+        if (!todos) {
+            return [];
+        }
+
+        return todos.toSorted(todoComparator);
+    }, [todos]);
 
     const { mutate: createTodo } = useCreateTodo();
     const { mutate: updateTodo } = useUpdateTodo();
@@ -15,7 +37,7 @@ export const HomePage = () => {
         <>
             <Header onItemAdd={createTodo}>To Do app</Header>
             <List>
-                {todos?.map((todo) => (
+                {sortedTodos.map((todo) => (
                     <ListItem
                         label={todo.label}
                         isDone={todo.isDone}
